4cf880bb84da6918b793fe903f1c67220284c3b8
Daniel Bälz <daniel.baelz@lysandor.de>
2014-10-15 18:14:17 +0200

    Add v4 support library and change activity to fragment activity

--- app/src/main/java/de/dbaelz/na42/MainActivity.java
+++ app/src/main/java/de/dbaelz/na42/MainActivity.java
@@ -1,12 +1,12 @@
 package de.dbaelz.na42;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
